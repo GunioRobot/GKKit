e51f8cbfa7eb514c11b5564dea5d292b90ce9f7d
Gaurav Khanna <gauravk92@gmail.com>
2010-08-27 18:31:38 -0400

    can't lazy build, must remove files when not needed :/

--- GKDataRequest.m
+++ GKDataRequest.m
@@ -6,16 +6,6 @@
 //
 
 #import "GKDataRequest.h"
-#import "ASIHTTPRequest.h"
-#import "ASIFormDataRequest.h"
-#import "ASIHTTPRequestDelegate.h"
-#import "XPathQuery.h"
-#import <libxml/tree.h>
-#import <libxml/parser.h>
-#import <libxml/HTMLparser.h>
-#import <libxml/xpath.h>
-#import <libxml/xpathInternals.h>
-#import <libxml/HTMLtree.h>
 
 @implementation GKDataRequest
 
