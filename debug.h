d84ad089dfa7cbb51fddc56f5243f474d9f36ef5
gauravk92 <gauravk92@gmail.com>
2011-01-16 16:18:05 -0500

    fixed DLOG_PREFIX bug in debug.h

--- debug.h
+++ debug.h
@@ -30,32 +30,35 @@
         #ifndef DLOG_PREFIX
             #define DLOG_PREFIX DL
         #endif
+        
+        #define Q_(x) #x
+        #define Q(x) Q_(x)
     
-        #define DLog(format, ...)       NSLog(@"DLOG_PREFIX:%@;", , [NSString stringWithFormat:[@" " stringByAppendingString:format], ## __VA_ARGS__ ])
-        #define DLogObject(Object)      NSLog(@"DLOG_PREFIX:%s:%@;", #Object , Object)
-        #define DLogNSObject(NSObject)  NSLog(@"DLOG_PREFIX:%s:%@;", #NSObject , NSObject)
-        #define DLogClass(Class)        NSLog(@"DLOG_PREFIX:%s:%@;", #Class, [NSString stringWithUTF8String:(class_getName(Class))])
-        #define DLogSEL(SEL)            NSLog(@"DLOG_PREFIX:%s:%@;", #SEL, NSStringFromSelector(SEL))
-        #define DLogRetain(Object)      NSLog(@"DLOG_PREFIX:%s:RET:%i;%s:%d", #Object, [Object retainCount], __PRETTY_FUNCTION__, __LINE__)
-        #define DLogCFRetain(CFObject)  NSLog(@"DLOG_PREFIX:%s:RET:%i;", #CFObject, CFGetRetainCount(CFObject))
-        #define DLogINT(int)            NSLog(@"DLOG_PREFIX:%s:%i;", #int, int)
-        #define DLogFLOAT(float)        NSLog(@"DLOG_PREFIX:%s:%f;", #float, float)
-        #define DLogBOOL(BOOL)          NSLog(@"DLOG_PREFIX:%s:%s;", #BOOL, (BOOL ? "TRUE" : "FALSE"))
+        #define DLog(format, ...)       NSLog(@"%s:%s:%@;", Q(DLOG_PREFIX), [NSString stringWithFormat:[@" " stringByAppendingString:format], ## __VA_ARGS__ ])
+        #define DLogObject(Object)      NSLog(@"%s:%s:%@;", Q(DLOG_PREFIX), #Object , Object)
+        #define DLogNSObject(NSObject)  NSLog(@"%s:%s:%@;", Q(DLOG_PREFIX), #NSObject , NSObject)
+        #define DLogClass(Class)        NSLog(@"%s:%s:%@;", Q(DLOG_PREFIX), #Class, [NSString stringWithUTF8String:(class_getName(Class))])
+        #define DLogSEL(SEL)            NSLog(@"%s:%s:%@;", Q(DLOG_PREFIX), #SEL, NSStringFromSelector(SEL))
+        #define DLogRetain(Object)      NSLog(@"%s:%s:RET:%i;%s:%d", Q(DLOG_PREFIX), #Object, [Object retainCount], __PRETTY_FUNCTION__, __LINE__)
+        #define DLogCFRetain(CFObject)  NSLog(@"%s:%s:RET:%i;", Q(DLOG_PREFIX), #CFObject, CFGetRetainCount(CFObject))
+        #define DLogINT(int)            NSLog(@"%s:%s:%i;", Q(DLOG_PREFIX), #int, int)
+        #define DLogFLOAT(float)        NSLog(@"%s:%s:%f;", Q(DLOG_PREFIX), #float, float)
+        #define DLogBOOL(BOOL)          NSLog(@"%s:%s:%s;", Q(DLOG_PREFIX), #BOOL, (BOOL ? "TRUE" : "FALSE"))
         #define DLogUIView(Object)      UILogViewHierarchy(Object)
         #if TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
-            #define DLogCGRect(CGRect)  NSLog(@"DLOG_PREFIX:%s:%@;",  #CGRect, NSStringFromCGRect(CGRect))
+            #define DLogCGRect(CGRect)  NSLog(@"%s:%s:%@;", Q(DLOG_PREFIX), #CGRect, NSStringFromCGRect(CGRect))
         #elif TARGET_OS_MAC 
-            #define DLogCGRect(CGRect)  NSLog(@"DLOG_PREFIX:%s:%@;", #CGRect, NSStringFromRect(NSRectFromCGRect(CGRect)))
+            #define DLogCGRect(CGRect)  NSLog(@"%s:%s:%@;", Q(DLOG_PREFIX), #CGRect, NSStringFromRect(NSRectFromCGRect(CGRect)))
         #endif
-        #define DLogFunc()              NSLog(@"DLOG_PREFIX:%s:%d;", __PRETTY_FUNCTION__, __LINE__)
+        #define DLogFunc()              NSLog(@"%s:%s:%d;", Q(DLOG_PREFIX), __PRETTY_FUNCTION__, __LINE__)
         
 #pragma mark -
 #pragma mark Class Method Logging
         
         #define DLogvoid()
-        #define DLogid(Object)      NSLog(@"DLOG_PREFIX:%s:%@;", #Object , Object)
-        #define DLogdouble(double)  NSLog(@"DLOG_PREFIX:%s:%f;", #double, double)
-        #define DLogint(int)        NSLog(@"DLOG_PREFIX:%s:%i;", #int, int)
+        #define DLogid(Object)      NSLog(@"%s:%s:%@;", Q(DLOG_PREFIX), #Object , Object)
+        #define DLogdouble(double)  NSLog(@"%s:%s:%f;", Q(DLOG_PREFIX), #double, double)
+        #define DLogint(int)        NSLog(@"%s:%s:%i;", Q(DLOG_PREFIX), #int, int)
         
         #define DLogMethodv0(selName) \
             - (void)selName { \
